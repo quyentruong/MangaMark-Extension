c32d1220cb6e3e61e0fd6cec49fba821c8fccbd2
Quyen Truong <quyen.truong0202@gmail.com>
2022-03-13 18:14:22 -0700

    fix small bug

---
+++ src/js/blockads.js
@@ -0,0 +1,22 @@
+if (url.includes("doctruyen3q")) {
+    simpleBlock("pc-banner");
+}
+
+if (url.includes("nhattruyen")) {
+    simpleBlock2("div.container.mrt5 > div.text-center.mrt5.mrb5");
+    simpleBlock2("div.container.mrt5 > div.row");
+}
+
+function simpleBlock(className) {
+    var ads = document.getElementsByClassName(className);
+    if (ads.length > 0) {
+        ads[0].remove();
+    }
+}
+
+function simpleBlock2(selector) {
+    var ads = document.querySelectorAll(selector);
+    if (ads.length > 0) {
+        ads[0].remove();
+    }
+}
